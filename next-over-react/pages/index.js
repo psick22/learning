a2449aeeffb04a26417ec5d83e61f88a6d8aa678
psick22 <kgirok@gmail.com>
2022-01-22 03:46:15 +0900

    why next-over-react

---
+++ next-over-react/pages/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <img src="/very-large-image.jpeg" width="1920" height="1080" alt="" />
+    </div>
+  );
+}
+
+// export function getStaticProps() {}
+
+// export function getStaticPaths() {}
---
+++ next-over-react/pages/tutorials/javascript.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const javascript = () => {
+  return <div>javascript page</div>;
+};
+
+export default javascript;
