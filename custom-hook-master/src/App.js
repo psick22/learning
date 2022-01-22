12863bf2b2d674031ac0188c84393fb70cd47856
psick22 <kgirok@gmail.com>
2022-01-23 05:48:33 +0900

    custom-hook

---
+++ custom-hook-master/src/App.js
@@ -0,0 +1,35 @@
+import { useRef, useState } from "react";
+import "./App.css";
+import useRandomJoke from "./useRandomJoke";
+
+function App() {
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const joke = useRandomJoke(firstName, lastName);
+
+  const generateJoke = (e) => {
+    e.preventDefault();
+    setFirstName(firstNameRef.current.value);
+    setLastName(lastNameRef.current.value);
+  };
+
+  return (
+    <div className="App">
+      <center>
+        <h1>Joke Generator</h1>
+        <h2>{joke}</h2>
+
+        <form>
+          <input placeholder="first name" ref={firstNameRef} />
+          <input placeholder="last name" ref={lastNameRef} />
+          <button onClick={generateJoke}>Generate</button>
+        </form>
+      </center>
+    </div>
+  );
+}
+
+export default App;
